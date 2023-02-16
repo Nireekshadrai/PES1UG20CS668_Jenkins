eb684546b2860efd467de9893d54bf261904e5d3
Nireeksha D Rai <76628362+Nireekshadrai@users.noreply.github.com>
2023-02-16 18:37:34 +0530

    Update pes1ug20cs668.cpp

--- pes1ug20cs668.cpp
+++ pes1ug20cs668.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(void) {
   cout << "Task 5 pipeline output\n";
-  cout<< "Nireeksha D Rai: PES1UG20CS668\n";
+  cout<< "Nireeksha D Rai: PES1UG20CS668\n"
   
   return 0;
 }
